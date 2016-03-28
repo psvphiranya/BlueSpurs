27e4241891eb3e3f8d3e2b89f026698a1706b3d9
psvphiranya <psvphiranya@gmail.com>
2016-03-28 18:22:31 -0300

    test1

---
+++ bluespurs-take-home-starter-kit-3dbc193617fa/src/main/java/com/bluespurs/starterkit/Application.java
@@ -0,0 +1,43 @@
+package com.bluespurs.starterkit;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EnableCaching
+
+public class Application implements ApplicationConfig {
+	private static final Price log = getPrice(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        final_quote = restTemplate.getForObject("", Application.class);
+        Price.info(quote.toString());
+    }
+}
+}
